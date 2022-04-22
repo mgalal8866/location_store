049b8e89a8bbc65313c2755e6b23e34a600c66b8
mgalal8866 <58759061+mgalal8866@users.noreply.github.com>
2022-04-22 05:05:06 +0200

    first commit

---
+++ app/Http/Controllers/CitysController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\citys;
+use Illuminate\Http\Request;
+
+class CitysController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\citys  $citys
+     * @return \Illuminate\Http\Response
+     */
+    public function show(citys $citys)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\citys  $citys
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(citys $citys)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\citys  $citys
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, citys $citys)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\citys  $citys
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(citys $citys)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_04_21_025149_create_branchs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBranchsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branchs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('store_id');
+			$table->string('name', 250);
+            $table->string('slug')->nullable();
+            $table->string('lat', 250);
+            $table->string('lng', 250);
+            $table->tinyInteger('active')->default('1');
+            $table->softDeletes();
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branchs');
+    }
+}
