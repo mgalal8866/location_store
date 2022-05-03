da3dee58621f70bc28888e59847a0e60b8bb05ce
mgalal <mgalal8866@gmail.com>
2022-05-03 05:11:51 +0200

    screes

--- resources/views/gen.blade.php
+++ resources/views/gen.blade.php
@@ -4,26 +4,25 @@
     {{-- <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.8.0/firebase-ui-auth.css" /> --}}
   </head>
   <body>
-    <!-- The surrounding HTML is left untouched by FirebaseUI.
-    Your app may use that space for branding, controls and other customizations.-->
+ 
     <h1>Welcome to My Awesome App</h1>
     <div id="firebaseui-auth-container"></div>
     <div id="loader">Loading...</div>
     <div id="uid"></div>
 
-    <script src="{{URL::asset('assets/jquery.min.js')}}"></script>
+     <script src="{{URL::asset('assets/jquery.min.js')}}"></script>
      <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js"></script>
      <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js"></script>
      {{-- <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script> --}}
    <script>
         var firebaseConfig = {
                 apiKey: '{{ env('apiKey') }}',
-                    authDomain: '{{ env('authDomain') }}',
-                    projectId: '{{ env('projectId') }}',
-                    storageBucket: '{{ env('storageBucket') }}',
-                    messagingSenderId: '{{ env('messagingSenderId') }}',
-                    appId: '{{ env('appId') }}',
-                    measurementId: '{{ env('measurementId') }}'
+                authDomain: '{{ env('authDomain') }}',
+                projectId: '{{ env('projectId') }}',
+                storageBucket: '{{ env('storageBucket') }}',
+                messagingSenderId: '{{ env('messagingSenderId') }}',
+                appId: '{{ env('appId') }}',
+                measurementId: '{{ env('measurementId') }}'
             };
 
             firebase.initializeApp(firebaseConfig);
