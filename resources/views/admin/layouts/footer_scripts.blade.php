e1623670a20bdf3d3240801a62e796aec7f68838
mgalal8866 <58759061+mgalal8866@users.noreply.github.com>
2022-05-12 16:53:02 +0200

    sss

--- resources/views/admin/layouts/footer_scripts.blade.php
+++ resources/views/admin/layouts/footer_scripts.blade.php
@@ -29,7 +29,7 @@
 <!-- AdminLTE App -->
 <script src="{{ URL::asset('assets/dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ URL::asset('assets/dist/js/demo.js') }}"></script>
+{{-- <script src="{{ URL::asset('assets/dist/js/demo.js') }}"></script> --}}
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 {{-- <script src="{{ URL::asset('assets/dist/js/pages/dashboard.js') }}"></script> --}}
 {{-- <script > document.body.classList.add('dark-mode');</script> --}}
--- resources/views/admin/layouts/main_sidebar.blade.php
+++ resources/views/admin/layouts/main_sidebar.blade.php
@@ -1,7 +1,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="{{ route('city') }}" class="brand-link">
+        <a href="{{ route('dashborad') }}" class="brand-link">
             <img src="{{ URL::asset('assets/pharm.png')}}" alt="Pharm Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
         </a>
