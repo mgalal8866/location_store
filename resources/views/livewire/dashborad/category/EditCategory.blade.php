a9908b01a773b39899ff7a6d54c60b951d14f3ef
mgalal <mgalal8866@gmail.com>
2022-05-18 03:59:38 +0200

    ssa

--- resources/views/livewire/dashborad/category/EditCategory.blade.php
+++ resources/views/livewire/dashborad/category/EditCategory.blade.php
@@ -24,7 +24,8 @@
                                 <div class="form-group">
                                     <label class="control-label">Select Image : </label>
                                         <div style="margin-bottom: 10px;">
-                                            <img src="{{$slug != null?$categorys->where('slug',$slug)->first()->image:'' }}" alt="favicon" style="max-width: 100px; max-height: 100px;">
+                                            {{-- {{$categorys->where('slug',$slug)->first()->image}} --}}
+                                            <img src="{{$slug != null?$categorys->where('slug',$slug)->first()->image??'' :'' }}" alt="favicon" style="max-width: 100px; max-height: 100px;">
                                         </div>
                                         <div class="display-block">
                                             <a class='btn btn-success btn-sm btn-file-upload'>
