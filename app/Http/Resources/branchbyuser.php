6bf294d60ac6a586899c281f807febee743c1259
mgalal <mgalal8866@gmail.com>
2022-05-26 04:31:21 +0200

    ss

--- app/Http/Resources/branchbyuser.php
+++ app/Http/Resources/branchbyuser.php
@@ -19,13 +19,32 @@ class branchbyuser extends JsonResource
             'star' => $this->top,
             'name' => $this->stores->name,
             'image' => $this->image,
+            'address' => $this->address??'',
+            'phone' => $this->phone,
+            'phone2' => $this->phone2,
+            'description' => $this->description??'',
+            'city' => $this->city->name,
+            'city_id' => $this->city_id,
+            'region' => $this->region->name,
+            'region_id' => $this->region_id,
+            'opentime' => $this->opentime??'',
+            'closetime' => $this->closetime??'',
             'rating' => round($this->rating, 2),
             'start_date' =>$this->start_date??'',
             'expiry_date' =>$this->expiry_date??'',
+            'lat' => $this->lat??'',
+            'lng' => $this->lng??'',
+            'visetor' => $this->view,
             'branch_num'=>$this->stores->branch_num . ' / '  . $this->count(),
             'product_num'=> $this->product_num . ' / ' . $this->product->count(),
             'accept'=>$this->acceptapi,
             'active'=>$this->activeapi,
+
+
+
+
+
+            // 'rating' => ($count != 0)?$sum/$count:0,
         ];
     }
 }
