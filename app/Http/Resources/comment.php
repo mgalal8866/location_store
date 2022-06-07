664040a13c096138e8969c0e8542215b7e59ec25
mgalal <mgalal8866@gmail.com>
2022-06-07 16:19:03 +0200

    dfasd

--- app/Http/Resources/comment.php
+++ app/Http/Resources/comment.php
@@ -9,11 +9,14 @@ class comment extends JsonResource
 
     public function toArray($request)
     {
+        // ini_set('precision', 17);
+
         return [
             'comment' => $this->comment??'',
             'user_id' => $this->user_id,
             'user_name' => $this->user->name,
-            'rating' => round($this->rating, 2)
+            'rating' =>  number_format((float)$this->rating, 2), //round($this->rating, 2) //
+            'precision' => ini_get('precision')
         ];
     }
 }
