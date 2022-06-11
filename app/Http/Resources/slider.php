84894e2eb2ce998eb1707b88d86a8388ddfdc89a
mgalal8866 <58759061+mgalal8866@users.noreply.github.com>
2022-06-11 14:49:23 +0200

    sas

--- app/Http/Resources/slider.php
+++ app/Http/Resources/slider.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\branchs;
+use App\Models\products;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class slider extends JsonResource
@@ -14,16 +16,28 @@ class slider extends JsonResource
      */
     public function toArray($request)
     {
+          $branch =  boolval($this->branchstate) ? branchs::find($this->branch_id) :'';
+          $product =  boolval($this->productstate) ? products::find($this->product_id):'';
+
+
         return
             [
                 'id'          => $this->id,
                 'image'       => $this->image,
                 'urlstate'    => boolval($this->urlstate),
                 'url'         => $this->url??'',
+
                 'branchstate'  =>  boolval($this->branchstate),
                 'branch_id'   => $this->branch_id??'',
+
                 'productstate'=>  boolval($this->productstate),
                 'product_id'     => $this->product_id??'',
+
+                'name' =>  $branch->stores->name ?? $product->branch->stores->name ?? '',
+                'lat' =>   $branch->lat ?? $product->branch->lat ?? '' ,
+                'lng' =>  $branch->lng?? $product->branch->lng ?? '' ,
+                'phone' =>  $branch->phone?? $product->branch->phone ?? '' ,
+
             ];
     }
 }
