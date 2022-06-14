1b9dd3f7befd11c1ec181a19f7e300fc54736830
mgalal <mgalal8866@gmail.com>
2022-06-14 03:28:45 +0200

    سشس

--- app/Http/Controllers/Api/BranchesController.php
+++ app/Http/Controllers/Api/BranchesController.php
@@ -160,8 +160,8 @@ class BranchesController extends Controller
     public function branchcheck(){
         $store =  stores::whereUserId(auth('api')->user()->id)->first();
         if( $store ==null){
-        return response()->json(['status' => true,'number' => '0']);
-    }
+          return response()->json(['status' => true,'number' => '0']);
+         }
         $num_branch =  $store->branch->count();
 
         if($store->branch_num >= $num_branch)
