d92b1eb58fbd54360d30aa97fef89de6eb63f02d
mgalal <mgalal8866@gmail.com>
2022-06-17 01:54:16 +0200

    zsa

--- app/Http/Resources/onebraches.php
+++ app/Http/Resources/onebraches.php
@@ -37,7 +37,7 @@ class onebraches extends JsonResource
             'lng'         => $this->lng,
             'visetor'     => $this->view,
             'rating'      => number_format(($count != 0)?$sum/$count:0 , 2),
-            'comments_number'    => $this->comment->count,
+            'comments_number'    => $this->comment->count(),
             'comments'    => comment::collection($this->comments),
             'product'     => product::collection($this->product->where('active',0))
         ];
