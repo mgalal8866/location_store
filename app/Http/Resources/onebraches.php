9f21ef807f7ed287eb507f74120638cfc67a402f
mgalal <mgalal8866@gmail.com>
2022-06-17 02:06:16 +0200

    khl'

--- app/Http/Resources/onebraches.php
+++ app/Http/Resources/onebraches.php
@@ -28,7 +28,7 @@ class onebraches extends JsonResource
             'image'       => $this->image,
             'description' => $this->description??'',
             'phone'       => $this->phone,
-            'phone2'      => $this->phone,
+            'phone2'      => $this->phone2,
             'city'        => $this->city->name,
             'region'      => $this->region->name,
             'opentime'    => $this->opentime??'',
