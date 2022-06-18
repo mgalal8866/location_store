a575b2c0602f5504b07105d79a20c38329c73b18
mgalal8866 <58759061+mgalal8866@users.noreply.github.com>
2022-06-18 12:59:18 +0200

    dfdsf

--- resources/views/admin/layouts/footer_scripts.blade.php
+++ resources/views/admin/layouts/footer_scripts.blade.php
@@ -61,7 +61,7 @@
                         "onclick": null,
                         "showDuration": "300",
                         "hideDuration": "1000",
-                        "timeOut": "15000",
+                        "timeOut": "3000",
                         "extendedTimeOut": "1000",
                         "showEasing": "swing",
                         "hideEasing": "linear",
