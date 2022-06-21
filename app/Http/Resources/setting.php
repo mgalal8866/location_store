da4b1e484b37aafff57c0826b3ceac917d563af3
mgalal <mgalal8866@gmail.com>
2022-06-21 23:05:42 +0200

    asa

--- app/Http/Resources/setting.php
+++ app/Http/Resources/setting.php
@@ -18,7 +18,7 @@ class setting extends JsonResource
         [
             'splash'           => $this->where('key','splash_screen')->select('value AS splash')->first()->splash,
             'phone'            => $this->where('key','phone_number')->select('value')->first()->value,
-            'maintenance_mode' => boolval($this->where('key','maintenance_mode')->select('value')->first()->value)??false,
+            'maintenance_mode' => (bool)$this->where('key','maintenance_mode')->select('value')->first()->value??false,
         ];
     }
 }
