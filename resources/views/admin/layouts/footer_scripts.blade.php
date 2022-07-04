c0134a005b4dc2491efd4af419dc425708e9f6a8
mgalal <mgalal8866@gmail.com>
2022-07-04 02:01:52 +0200

    sas

--- resources/views/admin/layouts/footer_scripts.blade.php
+++ resources/views/admin/layouts/footer_scripts.blade.php
@@ -125,6 +125,5 @@
 
  </script>
 
-
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9908015336564777"
      crossorigin="anonymous"></script>
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
+
   @include('admin.layouts.head')
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -38,6 +39,26 @@
 
     <!-- Main content -->
     <section class="content">
+        <amp-ad width="100vw" height="320"
+     type="adsense"
+     data-ad-client="ca-pub-9908015336564777"
+     data-ad-slot="1472048860"
+     data-auto-format="rspv"
+     data-full-width="">
+  <div overflow=""></div>
+</amp-ad>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9908015336564777"
+     crossorigin="anonymous"></script>
+<!-- sasas -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9908015336564777"
+     data-ad-slot="1472048860"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     @yield('content')
         @isset($slot)
         {{$slot}}
