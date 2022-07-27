265bc7f10e57bac8356d0a71733e8e4cef625bbb
mgalal <mgalal8866@gmail.com>
2022-07-27 23:08:05 +0200

    modified:   resources/views/Front/layout.blade.php

--- resources/views/Front/layout.blade.php
+++ resources/views/Front/layout.blade.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<html lang="en">
 <head>
-<!-- Website Template designed by www.downloadwebsitetemplates.co.uk -->
 <meta charset="UTF-8">
 <title>{{$setting->where('key','site_title')->first()->value??''}}</title>
 <meta name="description" content="{{$setting->where('key','site_description')->first()->value??''}}">
@@ -24,6 +20,10 @@
 <script src="{{ URL::asset('assets/Front/js/owlcarousel.js')}}"></script>
 <script src="{{ URL::asset('assets/Front/js/uikit.scrollspy.js')}}"></script>
 <script src="{{ URL::asset('assets/Front/js/scripts.js')}}"></script>
+    @inject('fonts', 'App\Models\fonts')
+    <?= !empty($fonts->whereIsDefault(1)->first()->font_url) ? $fonts->whereIsDefault(1)->first()->font_url : ''; ?>
+    <style>body { <?php echo $fonts->whereIsDefault(1)->first()->font_family; ;
+        ?> }</style>
 <!--[if lt IE 9]><script src="{{ URL::asset('https://html5shiv.googlecode.com/svn/trunk/html5.js')}}"></script><![endif]-->
 </head>
 
@@ -65,8 +65,6 @@
     </div>
 
 
-{{-- <div class="arrow-separator arrow-grey"></div> --}}
-
 
 <div class="arrow-separator arrow-white"></div>
 
@@ -100,16 +98,20 @@
 <div class="fullwidth colour2 clearfix">
 	<div id="maincont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
 
-		<p><strong>Lorem ipsum dolor</strong> sit amet, consectetur adipiscing elit. <a title="" href="#">Donec quis enim nulla</a>. Suspendisse posuere arcu ac eros iaculis egestas commodo risus tempus. Fusce placerat nulla nisi. Proin congue erat non orci adipiscing nec porttitor lacus egestas. <em>Donec venenatis</em>, risus et hendrerit ultrices, libero sem lacinia risus, ut convallis massa sapien nec libero. Suspendisse arcu orci, gravida nec aliquet ut, lacinia non nisl.</p>
-        <h2>Sign up and we'll let you know when we launch</h2>
-        <p>If you would like to receive <strong>news and special offers</strong> please send us your email address below:</p>
-		<div id="signupform" class="sb-search clearfix">
+                <p> دليلك الشامل الذى يحتوى على ارقام الطوارئ و الخدمات العامة و الخاصة
+        مثل المستشفيات و العيادات و الصيدليات و السوبر ماركت و المطاعم و البنوك وانشطة اخرة كثيرة
+        من خلال  <strong> دليل مصر </strong> يوضح لك ارقام التليفون و العنوان و مواعيد العمل 
+        و اللوكيشن لسهولة التوجة الية و عروض المتجر الجديدة
+        </p>
+       <h2> دليل مصر دليلك الشامل لكل ما تحتاج اليه </h2>
+         {{-- <p>If you would like to receive <strong>news and special offers</strong> please send us your email address below:</p> --}}
+		{{-- <div id="signupform" class="sb-search clearfix">
 			<form method="post" id="contact" class="clearfix">
 				<input class="sb-search-input" placeholder="Enter email ..." type="text" value="">
 				<input class="sb-search-submit" value="" type="submit">
 				<button class="formbutton" type="submit"><span class="fa fa-search"></span></button>
 			</form>
-		</div>
+		</div> --}}
 
 	</div>
 </div>
@@ -141,7 +143,6 @@
 	<div id="footercont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
         <center>
 		<p class="backtotop"><a title="" href="#backtotop"><span class="fa fa-angle-double-up"></span></a></p>
-
         <div id="socialmedia" class="clearfix">
 			<ul>
                 @foreach ( $setting->where('section','front') as  $set)
@@ -152,8 +153,7 @@
                     <li><a title="" href="#" rel="external"><span class="fa fa-twitter"></span></a></li>
                     @endif
                 @endforeach
-
-				</ul>
+			</ul>
 		</div>
 
             <strong>Copyright &copy; 2022 <a href="https://api.whatsapp.com/send?phone=01024346011">Mohamed Galal</a>.</strong>
